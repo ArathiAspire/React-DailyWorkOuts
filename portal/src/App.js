48571d509aec976e66712fd8e644a784016b3006
ArathiJayaprakash <arathiparambil@gmail.com>
2023-07-06 16:11:47 +0530

    measured performane using profiler

--- portal/src/App.js
+++ portal/src/App.js
@@ -2,22 +2,34 @@ import "./App.css";
 import { createPortal } from "react-dom";
 import NoPortalModal from "./NoPortalModal";
 import PortalModal from "./PortalModal";
+import { Profiler } from "react";
 
 function App() {
+  const onRender = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  ) => {
+    alert(`Component ${id} rendered in ${actualDuration} ms`);
+  };
   return (
     <>
       <h1>Portal</h1>
       <div style={{ border: "2px solid black" }}>
         <p>This is placed in the parent div</p>
-        {createPortal(
-          <p>This is placed in the doc body.</p>
-          , document.body)}
+        {createPortal(<p>This is placed in the doc body.</p>, document.body)}
       </div>
       <div>
-        <NoPortalModal/>
+        <Profiler id="NoPortalModal" onRender={onRender}>
+          <NoPortalModal />
+        </Profiler>
       </div>
       <div>
-        <PortalModal/>
+        <PortalModal />
       </div>
     </>
   );
