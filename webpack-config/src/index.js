df8bfb2c4e6349953098a0ec49a54a1acb6fe26c
ArathiJayaprakash <arathiparambil@gmail.com>
2023-07-21 15:16:26 +0530

    deleted unwanted

--- webpack-config/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./index.html"
-
-const appElement = document.getElementById("root");
-ReactDOM.render(<App />, appElement);
